ca291117655cc3cdc1e41cadb9efc1b095aefc0d
Tengyi <tengyi@hiringbranch.com>
2024-11-17 15:48:12 -0500

    add cors

--- AI_API/index.js
+++ AI_API/index.js
@@ -1,10 +1,26 @@
 // Import .env file
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 // set up express app
 const app = express();
 app.use(express.json());        // allows data in JSON format
+
+app.use(cors());                // allow all origins for now
+
+// const allowedOrigins = ['http://localhost:3000', 'https://roundone.onrender.com/'];
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
+
 
 // add routes
 import aiRoutes from './routes/ai.js';
